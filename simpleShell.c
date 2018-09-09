c419e0d5e54b738e7ad2c3b2210eadaebc14f9e5
Kasey Ann <kaseystowell@Kaseys-MBP.attlocal.net>
2018-09-09 16:00:48 -0400

    storing command as first text entry and storing params as all remaining text entries

--- simpleShell.c
+++ simpleShell.c
@@ -5,29 +5,53 @@
 #include <string.h>
 #include <sys/wait.h>
 
-/* program start */
+/* Simple command interpreter or shell. */
 int main() {
+
+	printf("Shell started. To quit please type 'quit'.\n");
+
 	while(1){
 
+		printf("gwsh: ");
 		pid_t pid;
 		int status;
-		char input[100]; 
-		char *word_1, *word_2;
+
+		char input[100];
+		char *params[15];
+		char *command;
 		char *done = "quit";
 
 		fgets(input, 100, stdin);
-		word_1 = strtok (input, " ");
-		word_2 = strtok (NULL, " "); 
+		char *split_string = strtok (input, " ");
 
-		if (*word_1 == *done){
-			exit(1);
+		/* Get the first initial command. */
+		command = split_string;
+		split_string = strtok (NULL, " ");
+
+		/* Get all arguments. */
+		int i = 0;
+		while (split_string != NULL) {
+			params[i++] = split_string;
+			split_string = strtok (NULL, " ");
+		};
+		params[i] = NULL;
+
+		/* Prints arguments for debugging. */
+		i = 0;
+		while (params[i] != NULL) {
+			printf("%s\n",params[i]);
+			i++;
 		}
 
+		/* Exit command to quit shell. */
+		if (*command == *done){
+			exit(0);
+		}
 
 		pid = fork();
 
 		if (pid < 0){
-			printf("it broke\n");
+			printf("Error: fork failed.\n");
 			exit(1);
 		}
 
@@ -40,8 +64,8 @@ int main() {
 			printf("about to execute. . . \n"); //for testing
 
 			char *cmd[3]; //trying stuff
-			cmd[0] = word_1;
-			cmd[1] = word_2;
+			cmd[0] = command;
+			//cmd[1] = params;
 			cmd[2] = NULL;
 
 			if (execvp(cmd[1], cmd) < 0) { 
