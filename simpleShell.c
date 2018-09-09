9c23d1619cf90944ce81a8191212a439b06ee618
Kasey Ann <kaseystowell@Kaseys-MBP.attlocal.net>
2018-09-09 18:33:02 -0400

    working shell

--- simpleShell.c
+++ simpleShell.c
@@ -8,70 +8,76 @@
 /* Simple command interpreter or shell. */
 int main() {
 
-	printf("Shell started. To quit please type 'quit'.\n");
+	/* Print intro! */
+	printf("\n  /\\___/\\\n ( o   o )\n (  =^=  ) \n (        )\n (         )\n (          )))))))))))\n\n");
+	printf("~* Welcome to tinyshell by kehlsey & kasey!\n");
+	printf("~* To quit please type 'quit'. \n\n");
 
 	while(1){
 
-		printf("gwsh: ");
+		printf("tinyshell ~ ");
 		pid_t pid;
 		int status;
 
 		char input[100];
 		char *params[15];
-		char *command;
 		char *done = "quit";
 
 		fgets(input, 100, stdin);
+
+		/* Handle new lines and spaces. */
+		while (input[0] == '\n' || input[0] == ' ')
+			fgets(input, 100, stdin);
+
+		/* Add null terminator. */
+		int len;
+		len = strlen(input);
+		if (len > 0 && input[len-1] == '\n')
+    		input[len-1] = '\0';
+
+		/* Store commands. */
 		char *split_string = strtok (input, " ");
-
-		/* Get the first initial command. */
-		command = split_string;
-		split_string = strtok (NULL, " ");
-
-		/* Get all arguments. */
 		int i = 0;
 		while (split_string != NULL) {
-			params[i++] = split_string;
+			params[i] = split_string;
 			split_string = strtok (NULL, " ");
+			i++;
 		};
 		params[i] = NULL;
 
 		/* Prints arguments for debugging. */
-		i = 0;
-		while (params[i] != NULL) {
-			printf("%s\n",params[i]);
-			i++;
-		}
+		// i = 0;
+		// while (params[i] != NULL) {
+		// 	printf("%s\n",params[i]);
+		// 	i++;
+		// }
 
 		/* Exit command to quit shell. */
-		if (*command == *done){
+		if (*params[0] == *done) {
+			printf("~* bye! :)\n");
 			exit(0);
-		}
+		};
 
 		pid = fork();
 
+		/* Catch failure and exit program. */
 		if (pid < 0){
 			printf("Error: fork failed.\n");
 			exit(1);
 		}
 
-		else if (pid){
-			printf("waiting. . . \n"); //for testing
-			wait(&status);
+		/* Parent waits for child. */
+		else if (pid) {
+			//printf("waiting. . . \n"); //for testing
+			waitpid(-1, &status, 0);
 		}
 
-		else{
-			printf("about to execute. . . \n"); //for testing
-
-			char *cmd[3]; //trying stuff
-			cmd[0] = command;
-			//cmd[1] = params;
-			cmd[2] = NULL;
-
-			if (execvp(cmd[1], cmd) < 0) { 
-				perror("exec failed");
-				exit(1);
-			}
+		/* This is the child, execute command. */
+		else {
+			//printf("about to execute. . . \n"); //for testing
+			execvp(params[0], params);
+			printf("tinyshell ~ Unknown command!\n");
+			exit(0);
 		}
 
 	}
