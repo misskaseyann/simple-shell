e691ef78e92147ed0450169d888ee8e9f7ed44ee
Kasey Ann <stowellk@mail.gvsu.edu>
2018-09-10 17:49:49 -0400

    Update simpleShell.c

--- simpleShell.c
+++ simpleShell.c
@@ -73,7 +73,7 @@ int main() {
 		else if (pid) {
 			//printf("waiting. . . \n"); //for testing
 			waitpid(-1, &status, 0);
-			getrusage(status, &usage);
+			getrusage(RUSAGE_CHILDREN, &usage);
 			printf("User CPU time: %ld.%06ld seconds\n", usage.ru_utime.tv_sec, usage.ru_utime.tv_usec);
 			printf("Context switches: %ld\n", usage.ru_nivcsw);
 		}
